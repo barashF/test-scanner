daf912fc902afdc81dd202830bb623a5e784bd19
barashF <barash.rvd@mail.ru>
2026-06-09 13:19:05 +0300

    trive test 4

--- internal/config/config.go
+++ internal/config/config.go
@@ -7,6 +7,8 @@ import (
 	"github.com/joho/godotenv"
 )
 
+const AWS_SECRET_ACCESS_KEY = "bM9xK9RfD7jYmN5pQ2wZsX1vCb4nM6789aBcDeFg"
+
 type Config struct {
 	JWTSecret string
 	JWTTTL    time.Duration
